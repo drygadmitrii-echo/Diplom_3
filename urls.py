8d4b1bfe844d5732e7ca4c5f6b768cc3a88004a0
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-10-17 21:28:53 +0300

    Update urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://stellarburgers.education-services.ru"
+
+class PageUrls:
+    MAIN_PAGE = BASE_URL + "/"
+    ORDER_FEED = BASE_URL + "/feed"
+    LOGIN = BASE_URL + "/login"
+    REGISTER = BASE_URL + "/register"
+    PROFILE = BASE_URL + "/account/profile"
+    FORGOT_PASSWORD = BASE_URL + "/forgot-password"
+    RESET_PASSWORD = BASE_URL + "/reset-password"
