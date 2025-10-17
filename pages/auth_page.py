a04a2f74f02350ed38630eddf9a26e941364c5a6
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-10-17 21:27:22 +0300

    Update auth_page.py

---
+++ pages/auth_page.py
@@ -0,0 +1,11 @@
+import allure
+from locators.auth_locators import AuthLocators
+from pages.base_page import BasePage
+
+
+class AuthPage(BasePage):
+    @allure.step("Выполняем авторизацию")
+    def authenticate(self, email, password):
+        self.fill_field(AuthLocators.email_input, email)
+        self.fill_field(AuthLocators.password_input, password)
+        self.click_element(AuthLocators.sign_in_btn)
